c9e36bf7238788de607dcc7601c4a7e25c864e13
Alexandre Iooss <erdnaxe@crans.org>
2019-01-24 10:34:21 +0100

    Smaller context processor and fix logs

--- med/context_processors.py
+++ med/context_processors.py
@@ -1,13 +1,5 @@
-from .settings import SITE_NAME
-
-
 def context_user(request):
-    user = request.user
-    is_perm = user.has_perms(['perm'])
-    is_bureau = user.has_perms(['bureau'])
     return {
-        'is_perm': is_perm,
-        'is_bureau': is_bureau,
-        'request_user': user,
-        'site_name': SITE_NAME,
+        'is_perm': request.user.has_perms(['perm']),
+        'is_bureau': request.user.has_perms(['bureau']),
     }
