5c60b90d9fc3c0e62d6d5337e14aac4fd920d19b
Alexandre Iooss <erdnaxe@crans.org>
2019-01-26 20:52:29 +0100

    Fix logout redirection and remove general views

--- med/urls.py
+++ med/urls.py
@@ -1,22 +1,18 @@
-from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
-
-from .views import index
+from django.views.generic import TemplateView
 
 # Customize Django Admin site
 admin.site.site_header = _('Med database administration')
 admin.site.site_title = _('Med Admin')
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls')),
-    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
     path('media/', include('media.urls')),
     path('search/', include('search.urls')),
     path('logs/', include('logs.urls')),
-    path('', index, name="index"),
+    path('', TemplateView.as_view(template_name='index.html'), name="index"),
 ]
