03f261b83f1e262157c9a00280d108b0a9fb99b7
Alexandre Iooss <erdnaxe@crans.org>
2019-01-27 18:01:27 +0100

    Translate URLs in English

--- media/urls.py
+++ media/urls.py
@@ -4,20 +4,25 @@ from . import views
 
 app_name = 'media'
 urlpatterns = [
-    path('auteurs/', views.AuthorsIndex.as_view(), name='index-auteurs'),
-    path('auteurs/add/', views.AuthorsCreate.as_view(), name='add-auteur'),
-    path('auteurs/<int:pk>/edit/', views.AuthorsUpdate.as_view(),
+    # Authors
+    path('authors/', views.AuthorsIndex.as_view(), name='index-auteurs'),
+    path('authors/add/', views.AuthorsCreate.as_view(), name='add-auteur'),
+    path('authors/<int:pk>/edit/', views.AuthorsUpdate.as_view(),
          name='edit-auteur'),
-    path('auteurs/<int:pk>/del/', views.AuthorsDelete.as_view(),
+    path('authors/<int:pk>/del/', views.AuthorsDelete.as_view(),
          name='del-auteur'),
-    path('auteurs/<int:pk>/history/', views.history,
+    path('authors/<int:pk>/history/', views.history,
          {'object': 'auteur'}, name='history'),
-    path('jeux/', views.GamesIndex.as_view(), name='index-jeux'),
-    path('jeux/add/', views.GamesCreate.as_view(), name='add-jeu'),
-    path('jeux/<int:pk>/edit/', views.GamesUpdate.as_view(), name='edit-jeu'),
-    path('jeux/<int:pk>/del/', views.GamesDelete.as_view(), name='del-jeu'),
-    path('jeux/<int:pk>/history/', views.history,
+
+    # Games
+    path('games/', views.GamesIndex.as_view(), name='index-jeux'),
+    path('games/add/', views.GamesCreate.as_view(), name='add-jeu'),
+    path('games/<int:pk>/edit/', views.GamesUpdate.as_view(), name='edit-jeu'),
+    path('games/<int:pk>/del/', views.GamesDelete.as_view(), name='del-jeu'),
+    path('games/<int:pk>/history/', views.history,
          {'object': 'jeu'}, name='history'),
+
+    # Media
     path('media/', views.MediaIndex.as_view(), name='index-medias'),
     path('media/add/', views.MediaCreate.as_view(), name='add-media'),
     path('media/<int:pk>/edit/', views.MediaUpdate.as_view(),
@@ -25,16 +30,18 @@ urlpatterns = [
     path('media/<int:pk>/del/', views.MediaDelete.as_view(), name='del-media'),
     path('media/<int:pk>/history/', views.history,
          {'object': 'media'}, name='history'),
+
+    # Borrowed media
     path('', views.MyBorrowedMediaIndex.as_view(),
          name='my-borrowed-media-index'),
-    path('emprunts/', views.AllBorrowedMediaIndex.as_view(),
+    path('borrowed/', views.AllBorrowedMediaIndex.as_view(),
          name='all-borrowed-media-index'),
-    path('emprunts/add/<int:userid>', views.add_emprunt, name='add-emprunt'),
-    path('emprunts/<int:pk>/retour/', views.retour_emprunt,
+    path('borrowed/add/<int:userid>', views.add_emprunt, name='add-emprunt'),
+    path('borrowed/<int:pk>/retour/', views.retour_emprunt,
          name='retour-emprunt'),
-    path('emprunts/<int:pk>/edit/', views.edit_emprunt, name='edit-emprunt'),
-    path('emprunts/<int:pk>/del/', views.BorrowedMediaDelete.as_view(),
+    path('borrowed/<int:pk>/edit/', views.edit_emprunt, name='edit-emprunt'),
+    path('borrowed/<int:pk>/del/', views.BorrowedMediaDelete.as_view(),
          name='del-emprunt'),
-    path('emprunts/<int:pk>/history/', views.history,
+    path('borrowed/<int:pk>/history/', views.history,
          {'object': 'emprunt'}, name='history'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,31 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'users'
 urlpatterns = [
-    url(r'^new_user/$', views.new_user, name='new-user'),
-    url(r'^edit_info/(?P<userid>[0-9]+)$', views.edit_info, name='edit-info'),
-    url(r'^state/(?P<userid>[0-9]+)$', views.state, name='state'),
-    url(r'^profile/(?P<userid>[0-9]+)$', views.profile, name='profil'),
-    url(r'^adherer/(?P<userid>[0-9]+)$', views.adherer, name='adherer'),
-    url(r'^mon_profil/$', views.mon_profil, name='profile'),
-    url(r'^add_clef/$', views.KeyCreate.as_view(), name='add-clef'),
-    url(r'^edit_clef/(?P<clefid>[0-9]+)$', views.KeyUpdate.as_view(), name='edit-clef'),
-    url(r'^del_clef/(?P<clefid>[0-9]+)$', views.del_clef, name='del-clef'),
-    url(r'^index_clef/$', views.index_clef, name='index-clef'),
-    url(r'^add_adhesion/$', views.MembershipCreate.as_view(), name='add-adhesion'),
-    url(r'^edit_adhesion/(?P<adhesionid>[0-9]+)$', views.MembershipUpdate.as_view(), name='edit-adhesion'),
-    url(r'^del_adhesion/(?P<adhesionid>[0-9]+)$', views.del_adhesion, name='del-adhesion'),
-    url(r'^index_adhesion/$', views.index_adhesion, name='index-adhesion'),
-    url(r'^$', views.index, name='index'),
-    url(r'^index_ajour/$', views.index_ajour, name='index-ajour'),
+    path('users/new/', views.new_user, name='new-user'),
+    path('edit_info/<int:userid>', views.edit_info, name='edit-info'),
+    path('state/<int:userid>', views.state, name='state'),
+    path('profile/<int:userid>', views.profile, name='profil'),
+    path('adherer/<int:userid>', views.adherer, name='adherer'),
+    path('profile/', views.mon_profil, name='profile'),
+    path('', views.index, name='index'),
+    path('index_ajour/', views.index_ajour, name='index-ajour'),
+
+    # Keys
+    path('keys/', views.index_clef, name='index-clef'),
+    path('keys/add/', views.KeyCreate.as_view(), name='add-clef'),
+    path('keys/<int:clefid>/edit/', views.KeyUpdate.as_view(),
+         name='edit-clef'),
+    path('keys/<int:clefid>/del/', views.del_clef, name='del-clef'),
+
+    # Membership
+    path('membership/', views.index_adhesion, name='index-adhesion'),
+    path('membership/add/', views.MembershipCreate.as_view(),
+         name='add-adhesion'),
+    path('membership/<int:adhesionid>/edit/',
+         views.MembershipUpdate.as_view(), name='edit-adhesion'),
+    path('membership/<int:adhesionid>/del/', views.del_adhesion,
+         name='del-adhesion'),
 ]
