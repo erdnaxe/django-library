1ba3b3b44d4dc09bbdb70f8607c114cc1461a39b
Alexandre Iooss <erdnaxe@crans.org>
2019-01-29 13:06:38 +0100

    Fix migration on SQLite

--- media/migrations/0004_translate.py
+++ media/migrations/0004_translate.py
@@ -6,6 +6,8 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
+    atomic = False
+
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('media', '0003_auto_20190127_1624'),
