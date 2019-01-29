02f0b767ef9097dd043a57950be53fda33ad76f4
Alexandre Iooss <erdnaxe@crans.org>
2019-01-29 11:41:42 +0100

    Use Django permissions in API

--- med/settings.py
+++ med/settings.py
@@ -181,7 +181,10 @@ def FILTERS_VERBOSE_LOOKUPS():
 # Rest framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissions',
+    )
 }
 
 # Number of item per page
